9cb0224101dac2244c8a33d0a324705f05e47fe9
Xenzey <Xenzey@209-3>
2024-11-20 09:23:25 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+try
+{
+    Console.WriteLine("Введите значение d: ");
+    double d = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение y: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+    double F = Math.Log(d) + (3.5 * d * d + 1) / Math.Cos(2 * y);
+    Console.WriteLine($"Результат F ={F}");
+}
+catch
+{
+    Console.WriteLine("Введите правильно!");
+}
